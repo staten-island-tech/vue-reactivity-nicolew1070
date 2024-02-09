d9aa24b5c0a563b0fd2fc7be533b8f4d8acf0366
nicolew88 <nicolew88@nycstudents.net>
2024-02-09 13:12:53 -0500

    Update HomeView.vue

--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -1,9 +1,59 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+const games = [
+  {
+    name: "Monolopy",
+    price: "$20",
+    img: "https://m.media-amazon.com/images/I/812z45q0jYL._AC_SX679_.jpg",
+  },
+  {
+    name: "Guess Who?",
+    price: "$16",
+    img: "https://m.media-amazon.com/images/I/61R9bZbcZWL._AC_SX679_.jpg",
+  },
+  {
+    name: "Sorry!",
+    price: "$10",
+    img: "https://m.media-amazon.com/images/I/91ABJdVWwyL._AC_SX679_.jpg",
+  },
+  {
+    name: "Trouble",
+    price: "",
+    img: "",
+  },
+  {
+    name: "Battleship",
+    price: "",
+    img: "",
+  },
+  {
+    name: "The Game of Life",
+    price: "",
+    img: "",
+  },
+  {
+    name: "Connect Four",
+    price: "",
+    img: "",
+  },
+  {
+    name: "Exploding Kittens",
+    price: "",
+    img: "",
+  },
+  {
+    name: "Poetry for Neanderthals",
+    price: "",
+    img: "",
+  },
+]  
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+</style>
